bc291e156d11c0d21e437b88288e35f19db0b30c
VeronicaBeMay-218 <veronica.19070007@itsmotul.edu.mx>
2023-03-20 19:08:59 -0600

    modif

--- src/App.vue
+++ src/App.vue
@@ -17,13 +17,13 @@ import Titulo from "./components/Titulo.vue";
       </div>
   
       <ul class="md:flex md:items-center z-[-1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500">
-        <li class="mx-4 my-6 md:my-0">
+        <li class="mx-4 my-6 md:my-0 px-4 py-1 text-black font-bold tracking-wider bg-gray-200 hover:bg-gray-800 rounded-full">
           <RouterLink to="/">Inicio</RouterLink>
           </li>
-        <li class="mx-4 my-6 md:my-0">
+        <li class="mx-4 my-6 md:my-0 px-4 py-1 text-black font-bold tracking-wider bg-gray-200 hover:bg-gray-800 rounded-full">
           <RouterLink to="/RegistroPersonas">Registro</RouterLink>
          </li>
-        <li class="mx-4 my-6 md:my-0">
+        <li class="mx-4 my-6 md:my-0 px-4 py-1 text-black font-bold tracking-wider bg-gray-200 hover:bg-gray-800 rounded-full">
           <RouterLink to="/ListaPersonas">Personas</RouterLink>
           </li>
 
